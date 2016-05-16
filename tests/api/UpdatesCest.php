06db0fa587079fe40c2943fa393ad6d35f9a8703
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2016-05-16 18:48:08 +0300

    UpdatesCest added feature since test

--- tests/api/UpdatesCest.php
+++ tests/api/UpdatesCest.php
@@ -14,7 +14,6 @@ class UpdatesCest extends BaseCest
     // tests
     public function tryToCheckUpdates(ApiTester $I)
     {
-        $I->wantTo('test checkUpdates');
         $I->sendGET('v2/checkUpdates');
         $I->seeResponseCodeIs(200);
         $I->seeResponseContainsJson(['idsForUpdate' => []]);
@@ -23,5 +22,18 @@ class UpdatesCest extends BaseCest
         $I->sendGET('v2/checkUpdates');
         $I->seeResponseCodeIs(200);
         $I->seeResponseContainsJson(['idsForUpdate' => [4]]);
+
+        $I->haveALevel(['name' => 'beginner']);
+        $I->sendGET('v2/checkUpdates');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson(['idsForUpdate' => [2, 4]]);
+    }
+
+    public function tryToCheckUpdatesWithFeatureSince(ApiTester $I)
+    {
+        $since = \Carbon\Carbon::parse('+5 hour');
+        $I->haveHttpHeader('If-modified-since', $since->toIso8601String());
+        $I->sendGET('v2/checkUpdates');
+        $I->seeResponseCodeIs(304);
     }
 }
