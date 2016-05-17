bc785476b0b5c2a99ffa8324f9cce34ec936e15d
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2016-05-17 12:01:13 +0300

    typo 'AnSpeaker' in SpeakersCest fix

--- tests/api/SpeakersCest.php
+++ tests/api/SpeakersCest.php
@@ -21,7 +21,7 @@ class SpeakersCest extends BaseCest
 
     public function tryToGetSpeaker(ApiTester $I)
     {
-        $I->haveAnSpeaker(['first_name' => 'test', 'last_name' => 'Speaker']);
+        $I->haveASpeaker(['first_name' => 'test', 'last_name' => 'Speaker']);
         $I->sendGET('v2/getSpeakers');
         $I->seeResponseCodeIs(200);
         $I->seeResponseContainsJson(['firstName' => 'test']);
@@ -30,7 +30,7 @@ class SpeakersCest extends BaseCest
     public function tryToGetSpeakerWithIfModifiedSince(ApiTester $I)
     {
         $since = \Carbon\Carbon::parse('-1 hour');
-        $I->haveAnSpeaker(['first_name' => 'test', 'last_name' => 'Speaker']);
+        $I->haveASpeaker(['first_name' => 'test', 'last_name' => 'Speaker']);
         $I->haveHttpHeader('If-modified-since', $since->toIso8601String());
         $I->sendGET('v2/getSpeakers');
         $I->seeResponseCodeIs(200);
@@ -40,7 +40,7 @@ class SpeakersCest extends BaseCest
     public function tryToGetSpeakerWithFeatureIfModifiedSince(ApiTester $I)
     {
         $since = \Carbon\Carbon::parse('+5 hour');
-        $I->haveAnSpeaker(['first_name' => 'test', 'last_name' => 'Speaker']);
+        $I->haveASpeaker(['first_name' => 'test', 'last_name' => 'Speaker']);
         $I->haveHttpHeader('If-modified-since', $since->toIso8601String());
         $I->sendGET('v2/getSpeakers');
         $I->seeResponseCodeIs(304);
