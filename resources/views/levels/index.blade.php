a43a3692c3795431d23f29fac993c327a61269a1
olexandr.spivak <olexandr.spivak@lemberg.co.uk>
2016-05-24 16:18:51 +0300

    fix: change index views

--- resources/views/levels/index.blade.php
+++ resources/views/levels/index.blade.php
@@ -24,19 +24,19 @@
 
                         <tbody>
                         <?php $i=0 ?>
-                        @foreach ($data as $level)
+                        @foreach ($data as $item)
                             <tr>
                                 <td>{{ ++$i }}</td>
                                 <td>
-                                    {{ $level->name }}
+                                    {{ $item->name }}
                                 </td>
                                 <td>
-                                    {{ $level->order }}
+                                    {{ $item->order }}
                                 </td>
                                 <td class="text-right">
-                                    <a href="{{ route('levels.show', ['id' => $level->id]) }}" class="btn btn-primary btn-xs"><i class="fa fa-folder"></i> {{ trans('View') }}</a>
-                                    <a href="{{ route('levels.edit', ['id' => $level->id]) }}" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> {{ trans('Edit') }}</a>
-                                    {!! Form::open(['url' => route('levels.destroy', ['id' => $level->id]), 'method' => 'POST', 'style' => 'vertical-align: middle; display: inline-block;']) !!}
+                                    <a href="{{ route('levels.show', ['id' => $item->id]) }}" class="btn btn-primary btn-xs"><i class="fa fa-folder"></i> {{ trans('View') }}</a>
+                                    <a href="{{ route('levels.edit', ['id' => $item->id]) }}" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> {{ trans('Edit') }}</a>
+                                    {!! Form::open(['url' => route('levels.destroy', ['id' => $item->id]), 'method' => 'POST', 'style' => 'vertical-align: middle; display: inline-block;']) !!}
                                         {{ method_field('DELETE') }}
                                         {{ Form::button("<i class='fa fa-trash-o'></i> ".trans('Delete'), ['type' => 'submit', 'class' => 'btn btn-danger btn-xs']) }}
                                     {!! Form::close() !!}
