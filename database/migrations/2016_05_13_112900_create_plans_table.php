4b5dac83b59cb4778391154600dc5248d3c6c87b
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2016-05-30 14:53:06 +0300

    update tests: Points and Floor Plans

--- database/migrations/2016_05_13_112900_create_plans_table.php
+++ database/migrations/2016_05_13_112900_create_plans_table.php
@@ -16,7 +16,7 @@ class CreatePlansTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('url');
-            $table->float('order');
+            $table->float('order')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
---
+++ tests/api/PointsCest.php
@@ -0,0 +1,61 @@
+<?php
+
+class PointsCest extends BaseCest
+{
+    public function _before(ApiTester $I)
+    {
+        parent::_before($I);
+    }
+
+    public function _after(ApiTester $I)
+    {
+        parent::_after($I);
+    }
+    // tests
+    public function tryToGetPointsWhenEmpty(ApiTester $I)
+    {
+        $I->sendGET('v2/getPOI');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson(['poi' => []]);
+    }
+
+    public function tryToGetPoint(ApiTester $I)
+    {
+        $I->haveAPoint(['name' => 'test']);
+        $I->sendGET('v2/getPOI');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson(['poiName' => 'test']);
+    }
+
+    public function tryToGetPointWithIfModifiedSince(ApiTester $I)
+    {
+        $since = \Carbon\Carbon::parse('-1 hour');
+        $I->haveAPoint(['name' => 'test']);
+        $I->haveHttpHeader('If-modified-since', $since->toIso8601String());
+        $I->sendGET('v2/getPOI');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson(['poiName' => 'test']);
+    }
+
+    public function tryToGetPointWithFutureIfModifiedSince(ApiTester $I)
+    {
+        $since = \Carbon\Carbon::parse('+5 hour');
+        $I->haveAPoint(['name' => 'test']);
+        $I->haveHttpHeader('If-modified-since', $since->toIso8601String());
+        $I->sendGET('v2/getPOI');
+        $I->seeResponseCodeIs(304);
+    }
+
+    public function tryToGetDeletedPoint(ApiTester $I)
+    {
+        $type = $I->haveAPoint(['name' => 'test']);
+        $I->sendGET('v2/getPOI');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson(['poiName' => 'test', 'deleted' => false]);
+        $type->delete();
+        $I->haveHttpHeader('If-modified-since', \Carbon\Carbon::now()->toIso8601String());
+        $I->sendGET('v2/getPOI');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson(['poiName' => 'test', 'deleted' => true]);
+    }
+}
