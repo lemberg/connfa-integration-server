59bf84446d3528313b21ce48bb8fcad9bf5b92d7
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2016-06-01 18:58:37 +0300

    fix: Request Validation formatting

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -27,24 +27,20 @@ class UserRequest extends Request
         $validation = [];
         if ($this->method() == "POST") {
             $validation = [
-                'name' => 'required|unique:users',
-                'email' => 'required|unique:users|email',
-                'password' => 'required|min:6|confirmed',
+                'name'                  => 'required|unique:users',
+                'email'                 => 'required|unique:users|email',
+                'password'              => 'required|min:6|confirmed',
                 'password_confirmation' => 'required|min:6',
             ];
         } elseif ($this->method() == "PUT") {
+            $validation = [
+                'name'  => 'required|unique:users,name,' . $this->route()->parameters()['users'] . ',id',
+                'email' => 'required|unique:users,email,' . $this->route()->parameters()['users'] . ',id|email',
+            ];
+
             if ($this->request->get('change_password') == true) {
-                $validation = [
-                    'name' => 'required|unique:users,name,' . $this->route()->parameters()['users'] . ',id',
-                    'email' => 'required|unique:users,email,' . $this->route()->parameters()['users'] . ',id|email',
-                    'password' => 'required|min:6|confirmed',
-                    'password_confirmation' => 'required|min:6',
-                ];
-            } else {
-                $validation = [
-                    'name' => 'required|unique:users,name,' . $this->route()->parameters()['users'] . ',id',
-                    'email' => 'required|unique:users,email,' . $this->route()->parameters()['users'] . ',id|email',
-                ];
+                $validation['password'] = 'required|min:6|confirmed';
+                $validation['password_confirmation'] = 'required|min:6';
             }
         }
 
