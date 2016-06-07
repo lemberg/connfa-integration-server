60e954d364814d2fbae5dddfe8d7809a80dab7b3
olexandr.spivak <olexandr.spivak@lemberg.co.uk>
2016-06-07 14:09:11 +0300

    [touch: 978] feature: dashboard page

---
+++ resources/views/partials/_event-article.blade.php
@@ -0,0 +1,10 @@
+<article class="media event">
+    <a class="pull-left date">
+        <p class="month">{{ $event->start_at->format("M") }}</p>
+        <p class="day">{{ $event->start_at->format("d") }}</p>
+    </a>
+    <div class="media-body">
+        <a class="title" href="{{ route($eventType.'.show', ['id' => $event->id ]) }}">{{ $event->name }}</a>
+        <p>{{ str_limit(strip_tags($event->text), 250) }}</p>
+    </div>
+</article>
