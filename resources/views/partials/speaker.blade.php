82b4a6a1d1ed458070dd9109ba4965634386c756
olexandr.spivak <olexandr.spivak@lemberg.co.uk>
2016-06-15 17:21:00 +0300

    [completed: 1048,1049] fix: disable icon twiter name and icon website if they empty, add 'no email' message to dashboard.

--- resources/views/partials/speaker.blade.php
+++ resources/views/partials/speaker.blade.php
@@ -4,17 +4,21 @@
             <div class="flex">
                 <ul class="list-inline widget_profile_box">
                     <li>
-                        <a href="{{ $speaker->website }}">
-                            <i class="fa fa-external-link"></i>
-                        </a>
+                        @if($speaker->website)
+                            <a href="{{ $speaker->website }}">
+                                <i class="fa fa-external-link"></i>
+                            </a>
+                        @endif
                     </li>
                     <li>
                         {{ Html::image(empty($speaker->avatar) ? '/assets/images/user.png' : $speaker->avatar, $speaker->fullname, ['class' => 'img-circle profile_img']) }}
                     </li>
                     <li>
-                        <a href="https://twitter.com/{{ $speaker->twitter_name }}" target="_blank">
-                            <i class="fa fa-twitter"></i>
-                        </a>
+                        @if($speaker->twitter_name)
+                            <a href="https://twitter.com/{{ $speaker->twitter_name }}" target="_blank">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                        @endif
                     </li>
                 </ul>
             </div>
@@ -23,7 +27,7 @@
             </a>
             <div class="flex">
                 <ul class="list-inline count2">
-                    {{ Html::email($speaker->email) }}
+                    {{ Html::email(empty($speaker->email) ? 'no email' : $speaker->email) }}
                 </ul>
             </div>
             <p>
