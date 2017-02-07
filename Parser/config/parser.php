54dc18159ac6b17cc2b9dda3d2c6de3f830d3f02
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2017-02-07 15:00:51 +0200

    [touch: 1463] config base url changed

--- Parser/config/parser.php
+++ Parser/config/parser.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'driver' => \Parser\Drivers\DrupalcampLondon::class,
-    'baseUrl' => 'http://drupalcamplondonstg.prod.acquia-sites.com/',
+    'baseUrl' => 'http://drupalcamp.london',
 ];
