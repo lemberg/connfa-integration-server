d0d209225473c283be63fa3fcffaf9d4cbe4bfab
pauk <volodymyr.pauk@lemberg.co.uk>
2017-03-17 16:42:00 +0200

    [touch: 1550] Fixed file size validation

--- app/Http/Requests/FloorRequest.php
+++ app/Http/Requests/FloorRequest.php
@@ -25,25 +25,31 @@ class FloorRequest extends Request
      */
     public function rules()
     {
+        $fileSize = 0;
+        if (ini_get('post_max_size') > ini_get('upload_max_filesize')) {
+            $fileSize = $this->convertSize(ini_get('upload_max_filesize'));
+        } else {
+            $fileSize = $this->convertSize(ini_get('post_max_size'));
+        }
         $validation = [];
         if (in_array($this->method(), ['POST', 'PUT'])) {
             $request = $this->request->all();
             if ($request['image-switch'] == 'image_url') {
                 $validation = [
                     'name' => 'required',
-                    'image_file' => 'mimes:jpeg,bmp,png,gif|max:20000',
+                    'image_file' => 'mimes:jpeg,bmp,png,gif|max:' . $fileSize,
                     'image_url' => 'required|url',
                 ];
             } elseif ($request['image-switch'] == 'image_file') {
                 $validation = [
                     'name' => 'required',
-                    'image_file' => 'required|mimes:jpeg,bmp,png,gif|max:20000',
+                    'image_file' => 'required|mimes:jpeg,bmp,png,gif|max:' . $fileSize,
                     'image_url' => 'url',
                 ];
             } else {
                 $validation = [
                     'name' => 'required',
-                    'image_file' => 'required|mimes:jpeg,bmp,png,gif|max:20000',
+                    'image_file' => 'required|mimes:jpeg,bmp,png,gif|max:' . $fileSize,
                     'image_url' => 'required|url',
                 ];
             }
@@ -51,4 +57,25 @@ class FloorRequest extends Request
 
         return $validation;
     }
+
+    /**
+     * @param string $value
+     * @return int
+     */
+    public function convertSize($value)
+    {
+        $value = trim($value);
+        $last = strtolower($value[strlen($value) - 1]);
+        switch($last) {
+            case 'g':
+                $value *= 1024;
+            case 'm':
+                $value *= 1024;
+            case 'k':
+                $value *= 1024;
+        }
+
+        return $value / 1024;
+    }
+
 }
--- app/Http/Requests/SpeakerRequest.php
+++ app/Http/Requests/SpeakerRequest.php
@@ -25,6 +25,12 @@ class SpeakerRequest extends Request
      */
     public function rules()
     {
+        $fileSize = 0;
+        if (ini_get('post_max_size') > ini_get('upload_max_filesize')) {
+            $fileSize = $this->convertSize(ini_get('upload_max_filesize'));
+        } else {
+            $fileSize = $this->convertSize(ini_get('post_max_size'));
+        }
         $validation = [];
         if (in_array($this->method(), ['POST', 'PUT'])) {
             $validation = [
@@ -32,7 +38,7 @@ class SpeakerRequest extends Request
                 'last_name' => 'required',
                 'website' => 'url',
                 'email' => 'between:3,64|email',
-                'image_file' => 'mimes:jpeg,bmp,png,gif|max:6000',
+                'image_file' => 'mimes:jpeg,bmp,png,gif|max:' . $fileSize,
                 'image_url' => 'url',
             ];
         }
@@ -51,4 +57,25 @@ class SpeakerRequest extends Request
             'website.url' => 'Not a valid URL format.',
         ];
     }
+
+    /**
+     * @param string $value
+     * @return int
+     */
+    public function convertSize($value)
+    {
+        $value = trim($value);
+        $last = strtolower($value[strlen($value) - 1]);
+        switch($last) {
+            case 'g':
+                $value *= 1024;
+            case 'm':
+                $value *= 1024;
+            case 'k':
+                $value *= 1024;
+        }
+
+        return $value / 1024;
+    }
+
 }
