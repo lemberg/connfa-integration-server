810e5fc979e2492fe91bde9267ed253f15f2ba11
pauk <volodymyr.pauk@lemberg.co.uk>
2017-06-21 17:41:10 +0300

    Install app page, change text

--- resources/views/schedule/share.blade.php
+++ resources/views/schedule/share.blade.php
@@ -32,7 +32,7 @@
             <div class="row">
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="buttons">
-                        <h3 class="text-center">Install app</h3>
+                        <h3 class="text-center">Dowload DrupalCon app first! Then you can add shared schedule.</h3>
                         <a href="{{ env('APP_LINK_GOOGLE_PLAY') }}">
                             <img src="{{ URL::asset('assets/images/google-play-icon.png') }}">
                         </a>
